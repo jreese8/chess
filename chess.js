34358920881d651fd9a84f1f2034ec0ebdacec52
Johnathan Reese <JohnathanReese8@gmail.com>
2023-01-31 16:30:13 -0500

    Now doing tutorial bt Tudor Todorescu since the logic of the pieces is more accurate

--- chess.js
+++ chess.js
@@ -1,16 +1,5 @@
-//Inserting images
-function insertImage() {
-    document.querySelectorAll('.box').forEach(image => { //selexts all elements in index.html
+import { piecesRender } from './services/piecesRender.service.js'
 
-        if (image.innerText.length !==0) { //!==0 menas if not zero, i.e. if no pieces are in the square, then the below if/else statement is ran
-            if (image.innerText == "Wpawn" || image.innerText == "Bpawn") {
-                image.innerHTML `${image.innerText} < img class=allImg allSpawn' src="${image.innerText}.png" alt="">`
-            }
-
-            else {
-                image.innerHTML =`${image.innerText} <img clas='allImg' src="${image.innerText}.png" alt="">`
-                image.style.cursor = 'pointer'
-            }
-        }
-    })
-}
+addEventListener( 'DOMContentLoaded', _ => {
+    piecesRender.renderPieces()
+})
